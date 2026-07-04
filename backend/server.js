e593beae7f4200a7a74875909f35cc96122363ae
ayush patel <152726075+risingstar185@users.noreply.github.com>
2026-07-04 16:53:30 +0530

    Update CORS settings for production environment

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors({
     origin: [
-        'http://localhost:5173',
-        'http://localhost:4173'
+        'https://intellmeet-1-zjen.onrender.com',
     ],
     credentials: true
 }));
@@ -57,8 +56,7 @@ connectDB();
 const io = new Server(server, {
     cors: {
         origin: [
-            'http://localhost:5173',
-            'http://localhost:4173'
+            'https://intellmeet-1-zjen.onrender.com',
         ],
         credentials: true
     }
